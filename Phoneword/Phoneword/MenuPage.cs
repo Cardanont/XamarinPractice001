51adc973aea84504f3f90d8637c0869bbce39a8c
Daniel Ontiveros <cardanont@gmail.com>
2020-04-22 22:05:39 -0500

    Issues with the menu page load fixed

--- Phoneword/Phoneword/MenuPage.cs
+++ Phoneword/Phoneword/MenuPage.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace Phoneword
@@ -8,14 +9,16 @@ namespace Phoneword
     public class MenuPage : ContentPage
     {
 
-        Button biblicalQ;
-        Button phoneT;
+        Button biblicalQButton;
+        Button phoneTButton;
+
+        string translatedNumber;
+
 
         public MenuPage()
         {
             this.Padding = new Thickness(20, 20, 20, 20);
 
-            
             StackLayout panel = new StackLayout
             {
                 Spacing = 15
@@ -28,12 +31,12 @@ namespace Phoneword
                 HorizontalTextAlignment = TextAlignment.Center
             });
 
-            panel.Children.Add(biblicalQ = new Button
+            panel.Children.Add(biblicalQButton = new Button
             {
                 Text = "Biblical questions"
             });
 
-            panel.Children.Add(phoneT = new Button
+            panel.Children.Add(phoneTButton = new Button
             {
                 Text = "Phone translator"
             });
@@ -45,15 +48,14 @@ namespace Phoneword
                 TextColor = Color.FromHex("#000000")
             });
 
+            //biblicalQButton.Clicked += Ontranslate;
+            phoneTButton.Clicked += GoToMain;
             this.BackgroundColor = Color.FromHex("#025951");
-
-            //biblicalQ.Clicked += new NavigationPage(new );
-            //phoneT.Clicked += GoToMain;
+            this.Content = panel;
         }
-
-        //public void GoToMain(object sender, EventArgs e)
-        //{
-        //    Application.Current.MainPage = new NavigationPage(new MainPage());
-        //}
+        public void GoToMain(object sender, EventArgs e)
+        {
+            Application.Current.MainPage = new NavigationPage(new MainPage());
+        }
     }
 }
