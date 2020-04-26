473321c25d69a13eb080b4a04dbbcc5a5856c26f
Daniel Ontiveros <cardanont@gmail.com>
2020-04-26 02:30:12 -0500

    Develop of questions logic

---
+++ Phoneword/Phoneword/BiblicalQLogic.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phoneword
+{
+    public class BiblicalQLogic
+    {
+
+        public string Question1(string answer)
+        {
+            switch (answer)
+            {
+                case "1":
+                    return "not true";
+
+                default:
+                    break;
+            }
+
+            return null;
+        }
+
+    }
+}
